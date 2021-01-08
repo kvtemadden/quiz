7b4634eb90f571d3bc8b996dddf121217a0019d5
kate <katy.madden99@gmail.com>
2021-01-08 21:36:26 +0000

    Built so that on click of start button, first question and answers show & timer goes from 60 -1 per second until 0

--- Develop/script.js
+++ Develop/script.js
@@ -1,62 +1,92 @@
 var secondsLeft = 60;
+var string = '';
+var ansList = document.createElement("ul");
+var title = document.getElementById("main-title");
+var info = document.getElementById("main-text");
+var countdown = document.getElementById("time-left");
+var quizContent = document.getElementById("content");
+var quizQuestions = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: "d"
+  }
+];
 
 var startBtn = document.getElementById("start");
 startBtn.addEventListener("click", startQuiz);
 
 function startQuiz() {
-      startTimer();
-      startBtn.className = "hide";
-      debugger;
-      firstQuestion();
+  startTimer();
+  startBtn.className = "hide";
+  debugger;
+  firstQuestion();
 }
 
-function startTimer () {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        countdown.textContent = secondsLeft;
-    
-    
-        if(secondsLeft <= 0) {
-          clearInterval(timerInterval);
-          speedRead();
-        }
-    
-      }, 1000);
+function startTimer() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    countdown.textContent = secondsLeft;
+
+
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
+    }
+
+  }, 1000);
 }
 
 function firstQuestion() {
-    var questionJSON = JSON.stringify(quizQuestions);
-    var questionString = JSON.parse(questionJSON);
-     title.textContent = questionString[0].question1;
+  var questionJSON = JSON.stringify(quizQuestions);
+  var questionString = JSON.parse(questionJSON);
+  title.textContent = questionString[0].question;
+  var answerJSON = JSON.stringify(questionString[0].answers);
+  var answerString = JSON.parse(answerJSON);
+  info.textContent = printObj(answerString);
+  info.appendChild(ansList);
 }
 
-var title = document.getElementById("main-title");
-var info = document.getElementsByTagName("p");
-var countdown = document.getElementById("time-left");
-var quizContent = document.getElementById("content");
-var quizQuestions = [
-    {
-      question1: "Who invented JavaScript?",
-      answers1: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      },
-    },
-    {
-      question2: "Which one of these is a JavaScript package manager?",
-      answers2: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-    },
-    {
-      question3: "Which tool can you use to ensure code quality?",
-      answers3: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-      },
+
+
+var printObj = function (ans) {
+  for (var letter in ans) {
+    if (typeof ans[letter] == 'string') {
+      string += letter + ': ' + ans[letter];
+      indvAns = document.createElement("li");
+  indvAns.innerHTML = string;
+  ansList.appendChild(indvAns);
+  string = '';
     }
-  ];
+    else {
+      string += letter + print(ans[letter]);
+      indvAns = document.createElement("li");
+  indvAns.innerHTML = string;
+  ansList.appendChild(indvAns);
+  string = '';
+    }
+  }
+
+}
